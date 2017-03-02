31e9fb030ce99e769af2a507ff09908d70e8cb52
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-03-02 20:30:24 +0000

    Separate SQL

---
+++ Red software/Red software/Database.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.Windows;
+
+namespace Red_software
+{
+    class Database
+    {
+    
+
+        public static void Test()
+        {
+            string connectionstring;
+            string filename = "database.sdf";
+            string password = "Red_software";
+
+
+            connectionstring = string.Format("datasource = \"{0}\"; password = '{1}'", filename, password);
+
+            if (!File.Exists(filename))
+            {
+                SqlCeEngine en = new SqlCeEngine(connectionstring);
+
+                en.CreateDatabase();
+            }
+
+            SqlCeConnection conn = new SqlCeConnection(connectionstring);
+            conn.Open();
+            SqlCeTransaction tx = conn.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+            SqlCeCommand cmd2 = new SqlCeCommand("create table elso(elso int, masodik int);", conn, tx);
+            SqlCeDataReader rdr2 = cmd2.ExecuteReader();
+            SqlCeCommand cmd1 = new SqlCeCommand("insert into elso values(66,99);", conn, tx);
+            SqlCeDataReader rdr1 = cmd1.ExecuteReader();
+            SqlCeCommand cmd = new SqlCeCommand("select * from elso;", conn, tx);
+            SqlCeDataReader rdr = cmd.ExecuteReader();
+
+            try
+            {
+                // iterate through the results
+                //
+                while (rdr.Read())
+                {
+                    int val1 = rdr.GetInt32(0);
+                    int val2 = rdr.GetInt32(1);
+                    MessageBox.Show(string.Format("{0}, {1}", val1, val2), "confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+
+                }
+            }
+            finally
+            {
+                // always call close when done reading
+                //
+                rdr.Close();
+
+                // always call close when done reading
+                //
+                conn.Close();
+            }
+        }
+    }
+}
--- Red software/Red software/MainWindow.xaml.cs
+++ Red software/Red software/MainWindow.xaml.cs
@@ -23,80 +23,14 @@ namespace Red_software
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class testBase
-        {
-            public virtual void test(int i = 15)
-            {
-                MessageBox.Show(string.Format("testBase.test: {0}", i), "confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            }
-        }
-        public class testDerived : testBase
-        {
-            public override void test(int i = 20)
-            {
-                MessageBox.Show(string.Format("testDerived.test: {0}", i), "confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            }
-        }
+
         public MainWindow()
         {
 
-            testBase T = new testDerived();
-
-            T.test();
-
-            //string connectionstring;
-            //string filename = "database.sdf";
-            //string password = "red_software";
-
-
-            //connectionstring = string.format("datasource = \"{0}\"; password = '{1}'", filename, password);
-
-            //if (!file.exists(filename))
-            //{
-            //    sqlceengine en = new sqlceengine(connectionstring);
-
-            //    en.createdatabase();
-            //}
-
-            //sqlceconnection conn = new sqlceconnection(connectionstring);
-            //conn.open();
-
-            //sqlcetransaction tx = conn.begintransaction(system.data.isolationlevel.readcommitted);
-
-            //sqlcecommand cmd2 = new sqlcecommand("create table elso(elso int, masodik int);", conn, tx);
-            //sqlcedatareader rdr2 = cmd2.executereader();
-            //sqlcecommand cmd1 = new sqlcecommand("insert into elso values(66,99);", conn, tx);
-            //sqlcedatareader rdr1 = cmd1.executereader();
-            //sqlcecommand cmd = new sqlcecommand("select * from elso;", conn, tx);
-            //sqlcedatareader rdr = cmd.executereader();
-
-            //try
-            //{
-            //    // iterate through the results
-            //    //
-            //    while (rdr.read())
-            //    {
-            //        int val1 = rdr.getint32(0);
-            //        int val2 = rdr.getint32(1);
-            //        // string val2 = rdr.getstring(1);
-
-            //        messagebox.show(string.format("{0}, {1}", val1, val2), "confirmation", messageboxbutton.yesno, messageboximage.question);
-
-
-            //    }
-            //}
-            //finally
-            //{
-            //    // always call close when done reading
-            //    //
-            //    rdr.close();
-
-            //    // always call close when done reading
-            //    //
-            //    conn.close();
-            //}
 
             InitializeComponent();
+
+            Database.Test();
         }
     }
 }
