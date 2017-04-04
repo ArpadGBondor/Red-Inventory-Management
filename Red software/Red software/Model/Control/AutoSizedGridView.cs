5dc233adbdbf88e8a17634e2b43cd27170aa2071
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-04-04 15:57:19 +0100

    Auto Sized GridView

---
+++ Red software/Red software/Model/Control/AutoSizedGridView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Red_Inventory_Management.Model
+{
+    public class AutoSizedGridView : GridView
+    {
+        HashSet<int> _autoWidthColumns;
+
+        protected override void PrepareItem(ListViewItem item)
+        {
+            if (_autoWidthColumns == null)
+            {
+                _autoWidthColumns = new HashSet<int>();
+
+                foreach (var column in Columns)
+                {
+                    if (double.IsNaN(column.Width))
+                        _autoWidthColumns.Add(column.GetHashCode());
+                }
+            }
+
+            foreach (GridViewColumn column in Columns)
+            {
+                if (_autoWidthColumns.Contains(column.GetHashCode()))
+                {
+                    if (double.IsNaN(column.Width))
+                        column.Width = column.ActualWidth;
+
+                    column.Width = double.NaN;
+                }
+            }
+
+            base.PrepareItem(item);
+        }
+    }
+}
